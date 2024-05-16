4c0d812ff51a819ade09e10f4d5af750de3dc361
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-05-16 21:34:20 +0200

    server and client component

---
+++ app/_components/Header.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import SearchBar from "./SearchBar"
+
+const Header = () => {
+  return (
+    <header>
+        <h3>Header</h3>
+        <Link href="/">HomePage</Link>
+        <Link href="/blog">Blog</Link>
+        <SearchBar/>
+    </header>
+  )
+}
+
+export default Header
